a7a46f64c1e58d735037a3fc5b424cddbe97a9b7
Jeff Stubler <brunsa2@gmail.com>
2010-07-03 15:55:14 -0500

    added login page

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8' />
+		<title>Login</title>
+	</head>
+	<body>
+		<form method="get" action="do_login.php">
+			<input name='username' placeholder='Username' />
+			<br />
+			<input type='password' name='password' placeholder='Password' />
+			<br />
+			<input type='submit' value='Login' />
+		</form>
+	</body>
+</html>
